284dc830657f18f1047edbfeeb1c7456b0d43da8
PacoJavoMx <franciscoj.alarcona@hotmail.com>
2025-05-19 20:06:08 -0600

    ascendente la consulta

--- resumen_boleto.php
+++ resumen_boleto.php
@@ -2,7 +2,7 @@
 $mysqli = new mysqli("156.67.64.3", "u332392237_wedding", "Temporal2025**", "u332392237_cecijaviaccess");
 $mysqli->set_charset("utf8");
 
-$query = "SELECT codigo_qr, nombre, mesa, personas, acceso_confirmado, fecha_entrada FROM boletos ORDER BY mesa, nombre";
+$query = "SELECT codigo_qr, nombre, mesa, personas, acceso_confirmado, fecha_entrada FROM boletos ORDER BY mesa, nombre ASC";
 $result = $mysqli->query($query);
 
 $data = [];
