99fc6df962cf68b2ed49c1b4b80e07c51a450052
PacoJavoMx <franciscoj.alarcona@hotmail.com>
2025-05-19 18:58:13 -0600

    actualizando conexion de bd

--- resumen_boleto.php
+++ resumen_boleto.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mmysqli("156.67.64.3", "u332392237_wedding", "Temporal2025**", "u332392237_cecijaviaccess");
+$mysqli = new mysqli("156.67.64.3", "u332392237_wedding", "Temporal2025**", "u332392237_cecijaviaccess");
 $mysqli->set_charset("utf8");
 
 $query = "SELECT codigo_qr, nombre, mesa, personas, acceso_confirmado, fecha_entrada FROM boletos ORDER BY mesa, nombre";
